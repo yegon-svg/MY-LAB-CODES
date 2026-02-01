68a3a4fe289e17a67f8262a85083c02e3c30ccd1
yegon-svg <yegonmeshack181@gmail.com>
2026-02-01 23:47:43 +0300

    Update guestion 2.py

--- guestion 2.py
+++ guestion 2.py
@@ -1,16 +1,15 @@
 import numpy as np
-import matplotlib . pyplot as plt
+import matplotlib.pyplot as plt
 
 R = 1000 # Ohms
-C = 10e-6 # Farads
+C = 1e-6 # Farads
 V = 5 # Volts
 
-
-t = np . linspace (0 , 0.05 , 1000) 
+t = np.linspace(0, 0.01, 1000)
 Vc = V * np . exp ( - t / (R * C ) ) 
-plt . plot (t , Vc )
-plt . xlabel (" Time (s)")
-plt . ylabel (" Voltage (V)")
-plt . title ("RC Discharging Curve ")
-plt . show ()
-
+        
+plt.plot(t, Vc)
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage (V)")
+plt.title("RC dis Charging Curve")
+plt.show()
