5623648607fd2eda753c8866a797a85a4c3f7982
yegon-svg <yegonmeshack181@gmail.com>
2026-02-01 19:23:13 +0300

    Update guestion 2.py

--- guestion 2.py
+++ guestion 2.py
@@ -6,11 +6,11 @@ C = 10e-6 # Farads
 V = 5 # Volts
 
 
-t = np . linspace (0 , 0.05 , 1000) # Increased time range to better visualize discharge
-Vc = V * np . exp ( - t / (R * C ) ) # Discharging capacitor formula
-
+t = np . linspace (0 , 0.05 , 1000) 
+Vc = V * np . exp ( - t / (R * C ) ) 
 plt . plot (t , Vc )
 plt . xlabel (" Time (s)")
 plt . ylabel (" Voltage (V)")
 plt . title ("RC Discharging Curve ")
 plt . show ()
+
