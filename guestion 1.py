7119fc2ad8530a5ffff62bffcc557b4e99e58f91
yegon-svg <yegonmeshack181@gmail.com>
2026-02-01 23:37:53 +0300

    Update guestion 1.py

--- guestion 1.py
+++ guestion 1.py
@@ -1,15 +1,14 @@
 while True:
     try:
-        voltage = float(input("Enter voltage (V):"))
+        voltage = float(input("Enter voltage (V): "))
         current = float(input("Enter current (I): "))
 
         power = voltage * current
-        print("Power (P) =", power, "Watts")
+        print(power, "Watts")
     except ValueError:
-        print("Invalid input! Please enter numbers only.")
+        print("")
+
     choice = input("Do you want to calculate again? (yes/no): ").lower()
     if choice != "yes":
-        print("Program ended.")
         break
-
 
